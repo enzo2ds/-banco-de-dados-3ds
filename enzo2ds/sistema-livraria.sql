699b2bda0151a4cb72df0ba1fbceaafe3c09bced
enzo2ds <enzo.kauan.carvalho@escola.pr.gov.br>
2026-03-27 08:35:32 -0300

    Update sistema-livraria.sql

--- enzo2ds/sistema-livraria.sql
+++ enzo2ds/sistema-livraria.sql
@@ -1,4 +1,5 @@
-DROP TABLE IF EXISTS Compras; -- Apague a que depende de Clientes primeiro
+-- 1. Limpar as tabelas existentes
+DROP TABLE IF EXISTS Compras;
 DROP TABLE IF EXISTS Clientes;
 
 CREATE TABLE Clientes (
@@ -7,9 +8,25 @@ CREATE TABLE Clientes (
     emailCliente VARCHAR(255) NOT NULL
 );
 
+
 CREATE TABLE Compras (
     CompraID INT PRIMARY KEY,
     ClienteID INT,
     NomeLivro VARCHAR(255) NOT NULL,
     FOREIGN KEY (ClienteID) REFERENCES Clientes(ID)
 );
+
+INSERT INTO Clientes (ID, nomeCliente, emailCliente) VALUES 
+(1, 'Carolina Maria de Jesus', 'carolina@exemplo.com'),
+(2, 'Machado de Assis', 'machado@exemplo.com'),
+(3, 'Clarice Lispector', 'clarice@exemplo.com');
+
+
+INSERT INTO Compras (CompraID, ClienteID, NomeLivro) VALUES 
+(101, 1, 'Quarto de Despejo'),
+(102, 2, 'Dom Casmurro'),
+(103, 3, 'A Hora da Estrela'),
+(104, 1, 'Casa de Alvenaria');
+
+SELECT * FROM Clientes;
+
